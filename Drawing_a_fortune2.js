86572ac74fc4859bab2cb3df0e0ea3de614a2dd1
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-18 08:53:38 +0900

    動作検証用

---
+++ Drawing_a_fortune2.js
@@ -0,0 +1,13 @@
+function omikuji() {
+    const hako = ["大吉", "中吉", "吉", "小吉", "凶", "大凶"];
+    const index = Math.floor(Math.random() * hako.length);
+    return hako[index];
+  }
+  
+  const button = document.querySelector("#button");
+  const output = document.querySelector("#output");
+  
+  button.addEventListener("click", function () {
+    const unsei = omikuji();
+    output.textContent = unsei;
+  });
